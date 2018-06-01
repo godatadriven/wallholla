8f430999781827457a57666ebf8143ed93feab4c
Vincent Warmerdam <vincentwarmerdam@Vincents-MacBook-Pro.local>
2018-06-01 14:11:58 +0200

    removed all old stuff

--- walholla/losses.py
+++
@@ -1,6 +0,0 @@
-import keras.backend as K
-
-def boosted_loss(y_true, y_pred):
-    when_i_dont_care = K.zeros(y_true.shape)
-    when_i_do_care = K.log(y_true) - K.log(y_pred)
-    return K.sum(K.where(y_true != y_pred, when_i_dont_care, when_i_do_care))
